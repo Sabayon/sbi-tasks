d35c72f11e02824052e5448cd6cc87f407026246
Ettore Di Giacinto <mudler@gentoo.org>
2018-07-22 22:15:31 +0200

    update housekeeping task

---
+++ housekeeping/prune.js
@@ -0,0 +1,11 @@
+{
+  "source": "https://gitlab.com/mudler/Mottainai-test",
+  "image": "spotify/docker-gc",
+  "directory": "/",
+  "script": "/docker-gc",
+  "task": "docker_execute",
+  "prune": "yes",
+  "planned": "@daily",
+  "environment" : ["REMOVE_VOLUMES=1", "FORCE_IMAGE_REMOVAL=1", "FORCE_CONTAINER_REMOVAL=1","GRACE_PERIOD_SECONDS=1"],
+  "binds": [ "/etc:/etc:ro" ]
+}
