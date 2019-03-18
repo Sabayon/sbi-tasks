fc877aade834a22f66df05f10abc2786d6b7f5e9
Ettore Di Giacinto <mudler@gentoo.org>
2019-03-18 17:07:04 +0100

    Adjust commit message for detect-obsolete

--- bots/detect-obsoletes/scripts/detect.sh
+++ bots/detect-obsoletes/scripts/detect.sh
@@ -5,7 +5,7 @@ OBSOLETES="$(sabayon-detectobsolete --arch $ARCH --overlay /root/repo 2>/dev/nul
 
 for i in $OBSOLETES; do
     git rm -r "$i" || true
-    git commit -m "[$i] Drop, using portage version" || true
+    git commit -m "[$i] Drop, unmaintained" || true
 done
 
 popd
