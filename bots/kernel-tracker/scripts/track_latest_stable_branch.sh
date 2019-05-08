e201f552996f5154d3932db90738944c642f3c2a
Ettore Di Giacinto <mudler@gentoo.org>
2019-05-08 17:47:13 +0200

    Also propose update of kernel-bumper task

--- bots/kernel-tracker/scripts/track_latest_stable_branch.sh
+++ bots/kernel-tracker/scripts/track_latest_stable_branch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+set -x
 export KTRACKER="${KTRACKER:-scripts/kernel-autobump-tracker}"
-
+export TASK="${TASK:-/root/repo/bots/kernel-tracker/kernel-bump.yaml}"
 containsElement () {
   local e match="$1"
   shift
@@ -19,9 +21,19 @@ EOF
     echo './kernel-autobump "${KERNEL_DIR}" stable origin '$major'.'$minor' sabayon- "'$major'\.'$minor'\.[0-9999]"' >> "$KTRACKER"
     git add "$KTRACKER"
     git commit -m "Track ${kernel}.x kernels"
+    export V="<version>"
+    emerge-webrsync
+    eix-sync || true
+    LAST_FW=${LAST_FW:-$(eix sys-kernel/linux-firmware -u --format '<bestversion:V>')}
+    LAST_KN=$(grep 'KERNEL_VERSION_BUMP=' $TASK | sed 's/.*=//g')
+    sed -i 's/^- FIRMWARE_VER=.*/- FIRMWARE_VER='$LAST_FW'/' $TASK
+    sed -i 's/^- KERNEL_VERSION_BUMP=.*/- KERNEL_VERSION_BUMP='$major'.'$minor'/' $TASK
+    sed -i 's/^- KERNEL_BASE=.*/- KERNEL_BASE='$LAST_KN'/' $TASK
+    git add $TASK
+    git commit -m "Update kernel-bumper task for ${major}.${minor}"
 }
 
-KERNEL_VERSIONS=${KERNEL_VERSIONS:-$(curl -s https://www.kernel.org/releases.json | jq '.releases[].version' -r | sed -nre 's/^([0-9]+\.)([0-9]+)\..*/\1\2/p')}
+KERNEL_VERSIONS=${KERNEL_VERSIONS:-$(curl -s https://www.kernel.org/releases.json | jq '.releases[].version' -r | sed -nre 's/^([0-9]+\.)([0-9]+).*/\1\2/p')}
 SAB_BRANCHES=${SAB_BRANCHES:-$(git ls-remote --heads https://github.com/Sabayon/kernel | sed -nre 's/.*refs\/heads\/(.*)/\1/p')}
 
 echo "Sabayon branches:" $SAB_BRANCHES
@@ -35,5 +47,6 @@ do
    else
     echo "[MISSING] We need to track $i"
     trackKernel $i
+    exit 0
    fi
 done
