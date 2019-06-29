7d2672d03d84f6ecbe243d61bac80de5c11e93dc
Ettore Di Giacinto <mudler@gentoo.org>
2019-06-29 13:50:34 +0200

    Add vars to point to different branches

--- bots/scripts/create_pr.sh
+++ bots/scripts/create_pr.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Assumes master is aligned up to upstream, and develop contains your changes 
 
+ORIGIN_BRANCH="${ORIGIN_BRANCH:-master}"
 pushd /root/repo
 git checkout $WORK_BRANCH
-COMMITS=$(git cherry master | awk '{ print $2 }')
+COMMITS=$(git cherry $ORIGIN_BRANCH | awk '{ print $2 }')
 for i in $COMMITS; do
-    git checkout master
+    git checkout $ORIGIN_BRANCH
     git checkout -b "branch-$i"
     git cherry-pick "$i"
     git push -v -f -u origin "branch-$i"
     # Branch is ready now to open PR
-    hub pull-request -m "$(git log -1 --pretty=%B)"
+    hub pull-request $HUB_ARGS -m "$(git log -1 --pretty=%B)"
     git checkout $WORK_BRANCH
 done
 popd
--- bots/scripts/setup_git_repo.sh
+++ bots/scripts/setup_git_repo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+RESET_BRANCH="${RESET_BRANCH:-master}"
 # Update user fork
 rm -rf /root/repo || true
 git clone $FORK_REPO /root/repo
@@ -8,11 +9,11 @@ cp ../scripts/create_pr.sh /create_pr.sh
 pushd /root/repo
 git remote add upstream $UPSTREAM_REPO
 git fetch --all
-git reset --hard upstream/master
+git reset --hard upstream/${RESET_BRANCH}
 git push -fv
 git branch -D $WORK_BRANCH || true
 git checkout -b $WORK_BRANCH
-git reset --hard upstream/master
+git reset --hard upstream/${RESET_BRANCH}
 git push -fv -u origin $WORK_BRANCH
 popd
 
