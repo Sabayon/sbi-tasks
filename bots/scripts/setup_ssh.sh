4f3c440c835d29b51e1dd2272d67b21908fdf491
Ettore Di Giacinto <mudler@gentoo.org>
2019-06-29 17:50:21 +0200

    bots: use sabayon/ci image

--- bots/scripts/setup_ssh.sh
+++ bots/scripts/setup_ssh.sh
@@ -7,7 +7,6 @@ ln -s $(pwd)/storage/hub /root/.config/hub
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa.pub
-equo i git hub > /dev/null 2>&1
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 
